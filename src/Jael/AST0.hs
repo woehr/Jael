37df88c7176e699e954412cf2678cdd566440b8d
Jordan Woehr <jordanwoehr@gmail.com>
2015-05-07 12:19:58 -0600

    Transform grammar to ast

--- src/Jael/AST0.hs
+++ src/Jael/AST0.hs
@@ -3,35 +3,89 @@
 module Jael.AST0 where
 
 import ClassyPrelude
+import qualified Data.Map as M
 
-import Jael.Grammar (Prog)
+import Jael.Grammar
 
 -- AST0 represents the top level definitions of a program by mapping
 -- their name to their type and expression.
 type AST0 = Map Text (Ty, Ex)
-
-toAST0 :: Prog -> Either Text AST0
-toAST0 p = Left "Error in toAST0"
-
+-- Perhaps this signature when adding poly-types?
+--type AST0 = Map Text (Either PolyTy Ty, Ex)
 
 data Lit  = LInt Integer
           | LFloat Double
             deriving (Show)
 
-data Ex = EVar String
+data Ex = EVar Text
         | ELit Lit
         | EApp Ex Ex
         | EAbs Text Ex
         | ELet Text Ex Ex
           deriving (Show)
 
-data Ty = TVar String
+data Ty = TVar Text
         | TInt
         | TFloat
+        | TChar
+        | TString
         | TBool
         | TFun Ty Ty
           deriving (Show)
 
-data PolyTy = PolyTy [Text] Ty
-              deriving (Show)
+--data PolyTy = PolyTy [Text] Ty
+--              deriving (Show)
 
+toAST0 :: GProg -> Either Text AST0
+toAST0 (GProg fns) =
+  let tyOf :: GType -> Ty
+      tyOf t = case t of
+                    GTyInt   -> TInt
+                    GTyFloat -> TFloat
+                    GTyChar  -> TChar
+                    GTyStr   -> TString
+                    --TyBool -> TBool
+
+      -- Helper to convert grammar to AST0 function type
+      mkTy :: [GArg] -> GType -> Ty
+      mkTy (a@(GArgTy argId argTy):as) retTy = TFun (tyOf argTy) (mkTy as retTy)
+      mkTy []     retTy = tyOf retTy
+
+      -- Helper to conver grammar to AST0 function expression
+      mkEx :: [GLet] -> GExpr -> Ex
+      mkEx (l@(GLet (Ident letId) letEx):ls) e = ELet (pack letId) (mkEx [] letEx) (mkEx ls e)
+      mkEx [] e = case e of
+                       GEVar    (Ident i) -> EVar (pack i)
+
+                       GEInt    i     -> ELit (LInt  i)
+                       GEIntNeg i     -> ELit (LInt (-i))
+
+--                       GEChar   c
+--                       GEStr    s
+
+                       GEPlus   e1 e2 -> EApp (EApp (EVar "+") (mkEx [] e1)) (mkEx [] e2)
+                       GEMinus  e1 e2 -> EApp (EApp (EVar "-") (mkEx [] e1)) (mkEx [] e2)
+                       GETimes  e1 e2 -> EApp (EApp (EVar "*") (mkEx [] e1)) (mkEx [] e2)
+      
+      -- Add a function to the symbol map if valid
+      addSym :: AST0 -> GFunc -> Either Text AST0
+      addSym acc (GFunc (Ident id) args ret lets expr) =
+        let argsMissingTypes = not . and . map (\a -> case a of
+                                                        GArgTy   _ _ -> True
+                                                        GArgUnty _   -> False
+                                               ) $ args
+            alreadyDefined = member (pack id) acc
+            insertInto = M.insert (pack id) ((mkTy args ret), (mkEx lets expr))
+        in  if      alreadyDefined   then Left $
+                      "Function \"" ++ pack id ++ "\" defined multiple times"
+            else if argsMissingTypes then Left $
+                      "Function \"" ++ pack id ++ "\" missing argument types"
+            else    Right $ insertInto acc
+
+  in  (foldM addSym M.empty fns) >>= checkMain
+
+  where
+      checkMain :: AST0 -> Either Text AST0
+      checkMain a = if member "main" a
+                       then Right a
+                       else Left  "No main function"
--- src/Main.hs
+++ src/Main.hs
@@ -3,33 +3,10 @@
 import ClassyPrelude
 
 import Jael.Grammar
---import Jael.Grammar.Abs (Prog)
---import Jael.Grammar.ErrM (Err)
---import Jael.Grammar.Par (pProg)
 import Jael.AST0
 
 data Env = Env {}
-
 type ParseFun a = [Token] -> Err a
-
-runParser :: ParseFun a -> Text -> Err a
-runParser p t = p (myLexer . unpack $ t) 
-
-parseProgram :: Text -> Either Text Prog
-parseProgram t = case runParser pProg t of
-                      Bad err  -> Left (pack err)
-                      Ok  tree -> Right tree
-
-pass1 :: Prog -> Either Text AST0
-pass1 = toAST0
-
-writeOutput :: Env -> AST0 -> Either Text (IO ())
-writeOutput e ast0 = Right $ hPutStrLn stdout $ tshow ast0
-
--- Compiles the text of a program.
--- Returns an error or an IO action that produces the desired output.
-compile :: Env -> Text -> Either Text (IO ())
-compile e p =  parseProgram p >>= pass1 >>= writeOutput e
 
 main :: IO ()
 main = do
@@ -38,3 +15,21 @@ main = do
        Left  err -> hPutStrLn stdout err
        Right res -> res
 
+-- Compiles the text of a program.
+-- Returns an error or an IO action that produces the desired output.
+compile :: Env -> Text -> Either Text (IO ())
+compile e p =  parseProgram p >>= pass1 >>= writeOutput e
+
+parseProgram :: Text -> Either Text GProg
+parseProgram t = case runParser pGProg t of
+                      Bad err  -> Left (pack err)
+                      Ok  tree -> Right tree
+
+runParser :: ParseFun a -> Text -> Err a
+runParser p t = p (myLexer . unpack $ t) 
+
+pass1 :: GProg -> Either Text AST0
+pass1 = toAST0
+
+writeOutput :: Env -> AST0 -> Either Text (IO ())
+writeOutput e ast0 = Right $ hPutStrLn stdout $ tshow ast0
