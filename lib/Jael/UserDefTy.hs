d9c051bac29c1e99c485b1c2fc1b5b1f7ec48c80
Jordan Woehr <jordanwoehr@gmail.com>
2015-10-02 09:33:55 -0600

    Fix out of date comment

--- lib/Jael/UserDefTy.hs
+++ lib/Jael/UserDefTy.hs
@@ -6,8 +6,8 @@ module Jael.UserDefTy where
 import ClassyPrelude
 import qualified Data.Set as S
 
--- User defined type, u, uniquely determines the grammar input, g, the type
--- of things added to an environment, t, and the error, e
+-- User defined type, u, grammar input, g, the type of things added to an
+-- environment, t, and the error, e
 class Show e => UserDefTy u g t e | u -> g t e
                                   , g -> u t e
                                   , e -> u g t
