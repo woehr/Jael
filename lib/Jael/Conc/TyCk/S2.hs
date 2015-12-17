fbe0448048e29fe47eb535f925ea836186268d5d
Jordan Woehr <jordanwoehr@gmail.com>
2015-12-17 12:54:22 -0700

    Fill in remaining s2 proc undefines. [ci skip]

--- lib/Jael/Conc/TyCk/S2.hs
+++ lib/Jael/Conc/TyCk/S2.hs
@@ -4,6 +4,7 @@ import qualified Data.Functor.Foldable as F
 import qualified Data.Map as M
 import           Jael.Conc.Proc
 import           Jael.Conc.Session
+import           Jael.Seq.AST
 import           Jael.Seq.Env
 import           Jael.Seq.TI.S2
 import           Jael.Seq.Types
@@ -17,7 +18,6 @@ type S2ProcErrM = Either [S2ProcErr]
 
 data S2ProcState = S2ProcState
                      { tpssEnv    :: HMTyEnv
-                     , tpssCount  :: Integer
                      , tpssErrs   :: [S2ProcErr]
                      , tpssS2PEx  :: [(Text, S2PEx)]
                      } deriving (Show)
@@ -26,7 +26,28 @@ type S2ProcM = State S2ProcState
 
 type SeqDefEnv = M.Map Text S1Ty
 
--- typeInf :: HMTyEnv -> S1Ex -> Either S2TypeErr S2TyEx
+addError :: S2ProcErr -> S2ProcM S2Proc
+addError e = modify (\s@(S2ProcState{..}) -> s{tpssErrs=e:tpssErrs}) >> return S2PNil
+
+s2ProcAddToEnv :: HMTypable a => [(Text, a)] -> S2ProcM ()
+s2ProcAddToEnv xs = do
+  hmEnv <- gets tpssEnv
+  case addToEnv hmEnv (map (second $ polyTy . hmTyOf) xs) of
+       Left dups -> addError (S2PERedefinedName dups) >> return ()
+       Right hmEnv' -> modify (\s@(S2ProcState{..}) -> s{tpssEnv=hmEnv'})
+
+checkSeq :: S1Ex -> S2ProcM S2TyEx
+checkSeq e = do
+  hmEnv <- gets tpssEnv
+  case typeInf hmEnv e of
+       -- TODO: Fix to not have an ugly error like this.
+       Left err -> addError (S2PEInferenceErr err) >> error "Should not hit because an error was added"
+       Right tyEx -> return tyEx
+
+parEnv :: [a] -> (a -> S2ProcM b) -> S2ProcM [b]
+parEnv as f = do
+  oldEnv <- gets tpssEnv
+  flip mapM as $ \a -> f a <* modify (\s@(S2ProcState{..}) -> s{tpssEnv=oldEnv})
 
 typeAndExtractSeq :: HMTyEnv
                   -> S1Proc
@@ -34,35 +55,64 @@ typeAndExtractSeq :: HMTyEnv
 typeAndExtractSeq env s1 =
   let alg :: S1ProcF (S2ProcM S2Proc)
                    -> S2ProcM S2Proc
-      alg (S1PGetChanF c1 c2 p)   = (liftM $ S2PGetChan c1 c2) p
-      alg (S1PGetValF c n p)      = (liftM $ S2PGetVal c n) p
-      alg (S1PGetIgnF c p)        = (liftM $ S2PGetIgn c) p
-      alg (S1PPutChanF c1 c2 p)   = (liftM $ S2PPutChan c1 c2) p
-      alg (S1PNewChanF n1 n2 s p) = (liftM $ S2PNewChan n1 n2 s) p
-      alg (S1PParF ps)            = (liftM S2PPar) (sequence ps)
-      alg (S1PCaseF c ps)         = (liftM $ S2PCase c)
-                                      (liftM (zip $ map fst ps)
-                                        (sequence $ map snd ps))
-      alg (S1PSelF c l p)         = (liftM $ S2PSel c l) p
+      alg (S1PGetChanF c1 c2 p)   = liftM (S2PGetChan c1 c2) p
+      alg (S1PGetIgnF c p)        = liftM (S2PGetIgn c) p
+      alg (S1PPutChanF c1 c2 p)   = liftM (S2PPutChan c1 c2) p
+      alg (S1PNewChanF n1 n2 s p) = liftM (S2PNewChan n1 n2 s) p
+      alg (S1PSelF c l p)         = liftM (S2PSel c l) p
       alg (S1PFwdF c1 c2)         = return $ S2PFwd c1 c2
       alg (S1PNilF)               = return S2PNil
 
-      alg (S1PPutValF c e p) = undefined
-      alg (S1PNewValF n e p) = undefined
-      alg (S1PCoRecF n as p) = undefined
-      alg (S1PNamedF n as)   = undefined
+      alg (S1PGetValF c n p) = s2ProcAddToEnv [(n, HMTyVar "a")] >> liftM (S2PGetVal c n) p
+
+      alg (S1PPutValF c e p) = do
+        tyEx <- checkSeq e
+        liftM (S2PPutVal c (S2PEx tyEx)) p
+
+      alg (S1PNewValF n e p) = do
+        tyEx <- checkSeq e
+        s2ProcAddToEnv [(n, tyEx)]
+        liftM (S2PNewVal n (S2PEx tyEx)) p
+
+      alg (S1PParF ps)    = liftM S2PPar (parEnv ps id)
+      alg (S1PCaseF c ps) = liftM (S2PCase c)
+                                  (liftM (zip $ map fst ps)
+                                         (parEnv (map snd ps) id))
+
+      alg (S1PCoRecF n as p) = do
+        -- Check the sequence arguments to a co-rec process. Return the variable
+        -- name and type in the first element of the tuple and the (ChanEx S2PEx)
+        -- in the second element.
+        xs <- parEnv as $ \(v, ce) ->
+              case ce of
+                   Left c -> return (Nothing, (v, Left c))
+                   Right e -> do
+                     tyEx <- checkSeq e
+                     return (Just (v, tyEx), (v, Right $ S2PEx tyEx))
+
+        -- Collect the first elements of xs which make up the sequential names
+        -- available to p.
+        let newEnvItems = catMaybes $ map fst xs
+
+        -- Replace hmEnv with the original with the sequential arguments added
+        modify (\s@(S2ProcState{..})->s{tpssEnv=env})
+        s2ProcAddToEnv newEnvItems
+
+        liftM (S2PCoRec n $ map snd xs) p
+
+      alg (S1PNamedF n as)   = liftM (S2PNamed n) $ parEnv as $
+        either (return . Left) (\e -> checkSeq e >>= return . Right . S2PEx)
 
       initState = S2ProcState { tpssEnv = env
-                              , tpssCount = 0
                               , tpssErrs = []
                               , tpssS2PEx = []
                               }
 
-      (s2, (S2ProcState{..})) = runIdentity (runStateT (F.cata alg s1)
-                                                       initState)
-   in if null tpssErrs
-         then return (s2, tpssS2PEx)
-         else throwError tpssErrs
+      (s2, (S2ProcState{tpssS2PEx=pEx, tpssErrs=es})) = runIdentity (runStateT (F.cata alg s1)
+                                                                               initState)
+   in if null es
+         then return (s2, pEx)
+         else throwError es
 
 inferS2Args :: [(Text, S1TyOrSess)] -> S2Proc -> M.Map Text S2PEx -> S2ProcErrM S2TopProc
 inferS2Args s1Args s2p mExpr = undefined
